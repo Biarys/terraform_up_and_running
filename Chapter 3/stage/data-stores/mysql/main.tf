560857c84d04696df2c6acd8966f48168a0e927f
Biarys <sbiarys@gmail.com>
2023-06-12 08:53:46 -0400

    chapter 3 end

--- Chapter 3/stage/data-stores/mysql/main.tf
+++ Chapter 3/stage/data-stores/mysql/main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "example" {
-  identifier_prefix = "terraform-up-and-running"
-  engine = "mysql"
-  allocated_storage = 10
-  instance_class = "db.t2.micro"
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
   skip_final_snapshot = true
-  db_name = "example_database"
+  db_name             = "example_database"
 
   username = var.db_username
   password = var.db_password
@@ -19,12 +19,12 @@ module "whatever" {
 }
 
 terraform {
-	backend "s3" {
-		bucket = "terraform-up-and-running-state-stupidawsrules"
-		key = "stage/data-stores/mysql/terraform.tfstate"
-		region = "us-east-2"
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-stupidawsrules"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
 
-		dynamodb_table = "terraform-up-and-running-locks"
-		encrypt = true
-	}
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
 }
--- Chapter 3/stage/services/webserver-cluster/outputs.tf
+++ Chapter 3/stage/services/webserver-cluster/outputs.tf
@@ -1,9 +1,4 @@
 output "alb_dns_name" {
-	value = aws_lb.example.dns_name
-	description = "The domain name of the load balancer"
-}
-
-output "dynamodb_table_name" {
-	value = aws_dynamodb_table.terraform_locks.name
-	description = "The name of the DynamoDB table"
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
